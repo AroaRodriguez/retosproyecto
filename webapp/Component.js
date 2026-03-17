18c3cee284954a7deb63664d300f51f98f407777
Aroa Rodriguez <aroa.rodriguez@udefine.net>
2026-03-17 12:22:40 +0000

    creacion del modelo JSON categoria

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,26 +1,48 @@
 sap.ui.define([
     "sap/ui/core/UIComponent",
-    "retosproyecto/model/models"
-], (UIComponent, models) => {
+    "sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
     "use strict";
 
     return UIComponent.extend("retosproyecto.Component", {
         metadata: {
-            manifest: "json",
-            interfaces: [
-                "sap.ui.core.IAsyncContentCreation"
-            ]
+            manifest: "json"
         },
 
-        init() {
-            // call the base component's init function
+        init: function () {
+            // 1. Llamar al init del padre (Imprescindible)
             UIComponent.prototype.init.apply(this, arguments);
 
-            // set the device model
-            this.setModel(models.createDeviceModel(), "device");
+            // 2. Aquí es donde ocurre la magia "fuera" del controlador
+            const oListModel = this.getModel("listModel");
 
-            // enable routing
+            // AttachRequestCompleted es el evento exacto del "momento de carga"
+            oListModel.attachRequestCompleted(function () {
+                this._prepareDynamicCategories(oListModel);
+            }.bind(this));
+
             this.getRouter().initialize();
+        },
+
+        _prepareDynamicCategories: function (oListModel) {
+            // Obtenemos los datos recién cargados
+            const aRequests = oListModel.getProperty("/SolicitudesSet");
+
+            if (aRequests && aRequests.length > 0) {
+                // Algoritmo para categorías únicas
+                const aUniqueCategories = [...new Set(aRequests.map(sol => sol.categoria))];
+                const aData = aUniqueCategories.map(cat => ({ categoria: cat }));
+
+                // 3. CREAMOS EL MODELO DINÁMICO A NIVEL GLOBAL
+                const oFilteredModel = new JSONModel({
+                    CategoriasSet: aData
+                });
+
+                // Al setearlo en el Componente ('this'), todas las vistas lo heredan
+                this.setModel(oFilteredModel, "filtradoModel");
+                
+                console.log("Categorías generadas globalmente desde Component.js");
+            }
         }
     });
 });
--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -2,110 +2,89 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
-    "sap/ui/model/json/JSONModel", 
+    "sap/ui/model/json/JSONModel",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator", 
+    "sap/ui/model/FilterOperator",
     "sap/m/MessageBox",
-    "sap/ui/core/Fragment"
-], function (Controller, MessageToast,JSONModel, Filter, FilterOperator, MessageBox, Fragment) {
+    "sap/ui/core/Fragment",
+    "sap/ui/core/util/File"
+
+], function (Controller, MessageToast, JSONModel, Filter, FilterOperator, MessageBox, Fragment, File) {
     "use strict";
 
     return Controller.extend("retosproyecto.controller.Home", {
-        
-        onInit: function () {  
+
+        onInit: function () {
+
         },
 
-        //Función para generar categorias del modelo 
-        generarCategorias: function (oListModel) {
-            //Obtenemos el array con todas las solicitudes de List.json
-            const aSolictiudes = oListModel.getProperty("/SolicitudesSet");
-            if(!aSolictiudes)return;
-            
-            //Mapeamos para sacar solo los nombres de categoría y usamos Set para limpiar duplicados
-            const aCategoriasUnicas = [...new Set (aSolictiudes.map (sol=>sol.categoria))];
-
-            //Formateamos el array para que lo entienda el comboBox
-            const aDatosComboBox = aCategoriasUnicas.map (cat => {
-                return {categoria: cat}; //array
-
-            })
-
-            const oFiltradoModel = new JSONModel ({
-                CategoriasSet: aDatosComboBox  
-            });
-
-            this.getView().setModel(oFiltradoModel,"filtrado");
-            
-        },
-
-
-
-        //Función filtros 
-        buscar: async function () {
-            //Declaración constantes
-            const sSolicitud = this.byId("inSolicitud").getValue();
-            const sTipologia = this.byId("inTipologia").getValue();
-            const sCategorizacion = this.byId("cbCategorizacion").getSelectedKey();
+        //Function search filter
+        searchMethod: async function () {
+            //Type constant and to declare
+            const sRequest = this.byId("inSolicitud").getValue();
+            const sTypology = this.byId("inTipologia").getValue();
+            const sCategory = this.byId("cbCategorizacion").getSelectedKey();
             const aFilters = [];
 
-            if (sSolicitud) {
-                // Creamos un filtro para el campo "id" de nuestro JSON (constructor Filter)
-                const oFilter = new Filter("id", FilterOperator.Contains, sSolicitud);
+            if (sRequest) {
+                // Create a filter for type ID our JSON file (used constructor filter)
+                const oFilter = new Filter("id", FilterOperator.Contains, sRequest);
                 aFilters.push(oFilter);
-            } 
-            if (sTipologia){
-                aFilters.push(new Filter("tipologia", FilterOperator.Contains, sTipologia));
-
-            } if (sCategorizacion) {
-               aFilters.push(new Filter("categoria", FilterOperator.EQ, sCategorizacion)); 
             }
-            
+            if (sTypology) {
+                aFilters.push(new Filter("tipologia", FilterOperator.Contains, sTypology));
+
+            } if (sCategory) {
+                aFilters.push(new Filter("categoria", FilterOperator.EQ, sCategory));
+            }
+
             const oTable = this.byId("idTable");
             const oBinding = oTable.getBinding("items");
-            oBinding.filter(aFilters); 
+            oBinding.filter(aFilters);
 
-            // Mensaje de feedback
-            if (sSolicitud) {
-                MessageToast.show("Buscando: " + sSolicitud);
+            // Message feedback
+            if (sRequest) {
+                MessageToast.show("Buscando: " + sRequest);
             } else {
                 MessageToast.show("Limpiando filtros...");
             }
-        },  //Modificar y añadir un nuevo modelo JSON
-
-        // Lógica del fragmento 
-        ayudaRequerida: async function (oEvent) {
-            //Guardamos el objeto Input que disparó el evento
-            this._oInputSource = oEvent.getSource(); 
-
-            // Cargamos el fragmento solo si no existe ya en memoria
-            if (!this._pAyudaDialog) {
-                this._pAyudaDialog = await this.loadFragment({
-                    name: "retosproyecto.fragment.DialogHelp"   
-                });
-                this.getView().addDependent(this._pAyudaDialog);
-            }    
-            // Abrimos el diálogo
-            this._pAyudaDialog.open();
         },
 
-        
-        
-        //Botón limpiar filtro clear
-        limpiarFiltro: function (){
+
+        // Function fragment
+        HelpRequest: async function (oEvent) {
+            //Save an object Input that trigger event 
+            this._oInputSource = oEvent.getSource();
+
+            // Load fragment only if not exists in memory 
+            if (!this._pHelpDialog) {
+                this._pHelpDialog = await this.loadFragment({
+                    name: "retosproyecto.fragment.DialogHelp"
+                });
+                this.getView().addDependent(this._pHelpDialog);
+            }
+            // Open dialog
+            this._pHelpDialog.open();
+        },
+
+
+
+        //Logic Button to clear filter
+        cleanFilter: function () {
             this.byId("inSolicitud").setValue("");
             this.byId("inTipologia").setValue("");
 
             this.buscar();
 
-            MessageToast.show("Filtros restablecidos");
-        }, 
+            MessageToast.show("{i18n>msgFilter}");
+        },
 
 
 
-         // Añadir los colores según tipologia
+        // Añadir los colores según tipologia
         formatState: function (sEstado) {
             if (!sEstado) return "None";
-            
+
             switch (sEstado) {
                 case "Aprobado": return "Success"; // Verde
                 case "Rechazado": return "Error";   // Rojo
@@ -118,136 +97,136 @@ sap.ui.define([
             MessageToast.show("Detalle de la solicitud seleccionado");
         },
 
-        //---------------------------------------------------OPERACIONES CRUD---------------------------------------------------------------------
-        
-        //Función CRUD Eliminar
-        eliminar: function(){
-            //Pasamos la tabla y los datos seleccionados
+        //---------------------------------------------------CRUD METHOD---------------------------------------------------------------------
+
+        //Function CRUD detele
+        delete: function () {
+            //Selected data and put in the table
             const oTable = this.byId("idTable");
             const aSelectedItems = oTable.getSelectedItems();
-            //Si no seleccionamos nada, mostrar un mensaje. 
-            if (aSelectedItems.length ===0) {
+            //If not selected nothing, show a message. 
+            if (aSelectedItems.length === 0) {
                 MessageToast.show("Selecciona al menos una solicitud para eliminar");
                 return;
             } else {
-                     //Al seleccionar, mostramos un mensaje si estamos seguros de eliminar.
-            MessageBox.confirm("Estás seguro que deseas eliminar los registros seleccionados?", {
-                onClose: function (sAction) {
-                    if (sAction === MessageBox.Action.OK) {
-                    const oModel = this.getView().getModel("listModel");
-                    const aData = oModel.getProperty("/SolicitudesSet");
+                //If selected, show a new message: would sure to delete.
+                MessageBox.confirm("Estás seguro que deseas eliminar los registros seleccionados?", {
+                    onClose: function (sAction) {
+                        if (sAction === MessageBox.Action.OK) {
+                            const oModel = this.getView().getModel("listModel");
+                            const aData = oModel.getProperty("/SolicitudesSet");
 
-                    //Filtramos para quedarnos con los que no están seleccionados:
-                    const aSelectContexts = aSelectedItems.map(oItem =>oItem.getBindingContext("listModel").getObject());
-                    const aNewData = aData.filter(oRow => !aSelectContexts.includes(oRow));
-                    //Pasamos los nuevos parametros    
-                    oModel.setProperty("/SolicitudesSet",aNewData);
-                    oTable.removeSelections();
-                    MessageToast.show("Eliminado con éxito");
+                            //We filter to keep those that are not selected:
+                            const aSelectContexts = aSelectedItems.map(oItem => oItem.getBindingContext("listModel").getObject());
+                            const aNewData = aData.filter(oRow => !aSelectContexts.includes(oRow));
+                            //New parametres    
+                            oModel.setProperty("/SolicitudesSet", aNewData);
+                            oTable.removeSelections();
+                            MessageToast.show("Eliminado con éxito");
 
-                    
-                } //Cierre IF
-            }.bind(this) //Cierre metodo onClose
-            }); //Cierre messageBox
+
+                        } //close IF
+                    }.bind(this) //close method closeDialog
+                }); //close messageBox
             }
-            }, //cierre función
-            
-            //Función CRUD crear
-            crear: function (){
-             const oView = this.getView();
+        }, //close function
+
+        //Function create CRUD
+        create: function () {
+            const oView = this.getView();
             const oModel = this.getView().getModel("listModel");
 
-                //Creamos el dialog:
-                if (!this._oDialog) {
-                    Fragment.load({
-                        id: oView.getId(), 
-                        name: "retosproyecto.fragment.NewList",
-                        controller: this
-                    }) .then(function (oDialog) {
-                        this._oDialog = oDialog;
-                        oView.addDependent(this._oDialog);
-                        this._oDialog.setBindingContext(null, "listModel"); // Modo Crear
-                        this._oDialog.open();
-                    }.bind(this));
-                } else {
-                    this._oDialog.setBindingContext(null, "listModel");
+            //Create the dialog:
+            if (!this._oDialog) {
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "retosproyecto.fragment.NewList",
+                    controller: this
+                }).then(function (oDialog) {
+                    this._oDialog = oDialog;
+                    oView.addDependent(this._oDialog);
+                    this._oDialog.setBindingContext(null, "listModel"); // Mode create
                     this._oDialog.open();
-                }
-            }, 
-            //Cerrar el dialogo sin introducir nada.
-            CerrarDialogNuevo: function(){
-                this._oDialog.close();
-            }, 
+                }.bind(this));
+            } else {
+                this._oDialog.setBindingContext(null, "listModel");
+                this._oDialog.open();
+            }
+        },
+        //Function close dialog if you not put information
+        newDialogClose: function () {
+            this._oDialog.close();
+        },
 
-            //Guardar el dialogo
-            GuardarNuevo: function () {
-                const oModel = this.getView().getModel("listModel");
-                const oContext = this._oDialog.getBindingContext("listModel");
-                
-                if (!oContext) {
+        //Function CRUD Save dialog
+        newSave: function () {
+            const oModel = this.getView().getModel("listModel");
+            const oContext = this._oDialog.getBindingContext("listModel");
+
+            if (!oContext) {
                 const aData = oModel.getProperty("/SolicitudesSet");
-                const oNuevaSolicitud = {
-                    id: this.byId("newId").getValue(), 
-                    tipologia: this.byId("newTipologia").getValue(),
-                    categoria: this.byId("newCategoria").getValue(),
-                    estado: this.byId("newEstado").getValue(), 
-                    precio: parseFloat(this.byId("newPrecio").getValue()) || 0
+                const oNewRequest = {
+                    id: this.byId("newId").getValue(),
+                    tipologia: this.byId("newTypology").getValue(),
+                    categoria: this.byId("newCategory").getValue(),
+                    estado: this.byId("newStatus").getValue(),
+                    precio: parseFloat(this.byId("newPrice").getValue()) || 0
                 };
 
-                if (!oNuevaSolicitud.id) {
+                if (!oNewRequest.id) {
                     MessageToast.show("El ID es obligatorio");
                     return;
                 }
 
-                //Agregamos al array y actualizamos el modelo listModel.json:
-                aData.push(oNuevaSolicitud);
+                //We add to the array and update the model listModel.json
+                aData.push(oNewRequest);
                 oModel.setProperty("/SolicitudesSet", aData);
-                MessageToast.show("Solicitud " + oNuevaSolicitud.id + " creada con éxito");
+                MessageToast.show("Solicitud " + oNewRequest.id + " creada con éxito");
 
-                MessageToast("Solicitud" + oNuevaSolicitud + "creada correctamente");
+                MessageToast("Solicitud" + oNewRequest + "creada correctamente");
             } else {
                 MessageToast.show("Solicitud " + oContext.getProperty("id") + " actualizada");
             }
 
-                //Cerramos el dialogo:
-                this.CerrarDialogNuevo();
-            }, 
+            //Call the function close dialog
+            this.newDialogClose();
+        },
 
 
-            //Función CRUD editar
-            editar: function(){
+        //Function CRUD edit
+        edit: function () {
 
-                const oTable = this.byId("idTable");
-                const aSelectedItems = oTable.getSelectedItems();
-                
-                //Si selecciona más de una fila (es decir, el selected es diferente a 1)
-                if (aSelectedItems.length !==1) {
+            const oTable = this.byId("idTable");
+            const aSelectedItems = oTable.getSelectedItems();
 
-                    MessageToast.show("Por favor, selecciona exactamente una fila para editar");
-                    return;
-                }
+            //If you select more than one row (that is, the selected is different from 1)
+            if (aSelectedItems.length !== 1) {
 
-                const oSelectedItem = aSelectedItems[0];
-                const oContext = oSelectedItem.getBindingContext("listModel");
-                const oView = this.getView();
+                MessageToast.show("Por favor, selecciona exactamente una fila para editar");
+                return;
+            }
 
-                if (!this._oDialog) {
-                    Fragment.load({
-                        id: oView.getId(), 
-                        name: "retosoproyecto.fragment.NewList",
-                        controller: this
-                    }).then(function (oDialog) {
-                        this._oDialog = oDialog;
-                        oView.addDependent(this._oDialog);
-                        //OJO!! enlazamos dialogo con la vista seleccionada
-                        this._oDialog.setBindingContext(oContext, "listModel");
-                        this._oDialog.open();   
-                    } .bind(this));
+            const oSelectedItem = aSelectedItems[0];
+            const oContext = oSelectedItem.getBindingContext("listModel");
+            const oView = this.getView();
 
-                } else {
+            if (!this._oDialog) {
+                Fragment.load({
+                    id: oView.getId(),
+                    name: "retosoproyecto.fragment.NewList",
+                    controller: this
+                }).then(function (oDialog) {
+                    this._oDialog = oDialog;
+                    oView.addDependent(this._oDialog);
+                    //Warning!! Linking the dialog with the selected view
                     this._oDialog.setBindingContext(oContext, "listModel");
                     this._oDialog.open();
-                }
+                }.bind(this));
+
+            } else {
+                this._oDialog.setBindingContext(oContext, "listModel");
+                this._oDialog.open();
             }
-    }); // Cierre del Controller.extend
-}); // Cierre del sap.ui.define
+        }
+    }); // close Controller.extend
+}); // close sap.ui.define
