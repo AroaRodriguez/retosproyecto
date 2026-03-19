1b6a5cccab1776367a52cba9b31983732b2b6129
Aroa Rodriguez <aroa.rodriguez@udefine.net>
2026-03-19 10:53:25 +0000

    Creada nueva pagina y la ruta

---
+++ webapp/controller/Detail.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/routing/History" // Librería obligatoria para el botón "Atrás"
+], function (Controller, History) {
+    "use strict";
+
+    return Controller.extend("retosproyecto.controller.Detail", {
+
+        onInit: function () {
+        },
+    });
+});
